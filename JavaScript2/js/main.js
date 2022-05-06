76c1a609d498f5a3d22c8107f312d642557c54d7
mashico1001 <92191079+mashico1001@users.noreply.github.com>
2022-05-06 23:13:51 +0900

    [5/6]クラスの継承まで

--- JavaScript2/js/main.js
+++ JavaScript2/js/main.js
@@ -280,24 +280,49 @@
 
 // show(posts[0]);
 
-class Post {
+class Post { //親クラス
   constructor(text) {
     this.text = text;
     this.likeCount = 0;
   }
 
   show() {
-    console.log(`${this.text} - ${this.likeCount}いいね`);
+    console.log(`${this.text} - ${this.likeCount}likes`);
   }
+
+  like() {
+    this.likeCount++;
+    this.show();
+  }
+
+  //静的メソッド…インスタンスを介さず、処理をする。
+  // //thisは使えない。
+  // static showInfo() {
+  //   console.log('Post class version 1.0');
+  // }
 }
 
+class SponsoredPost extends Post { //子クラス
+  constructor(text, sponsor) {
+    super(text);
+    this.sponsor = sponsor;
+  }
 
+  show() {
+    super.show();
+    console.log(`... sponsored by ${this.sponsor}`);
+  }
+
+}
 
 const posts = [
   new Post('JavaScriptの勉強中…'),
   new Post('プログラミング楽しい'),
+  new SponsoredPost('3分動画でマスターしよう', 'dotinstall'),
 ];
 
-posts[0].show();
-posts[1].show();
+// posts[0].like();
+posts[2].show();
+posts[2].like();
+
 }
