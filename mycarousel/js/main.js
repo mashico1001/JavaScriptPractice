4ca2856fb60a54a7816e22e10ae8a1f291d62df1
mashico1001 <92191079+mashico1001@users.noreply.github.com>
2022-05-15 15:42:33 +0900

    [5/15]ドットインストール、カルーセル編完了

--- mycarousel/js/main.js
+++ mycarousel/js/main.js
@@ -1,5 +1,75 @@
 'use strict';
 
 {
-  
+  const next = document.getElementById('next');
+  const prev = document.getElementById('prev');
+  const ul = document.querySelector('ul');
+  const slides = ul.children;
+  // 画像の幅の分のwidthだけ取得する。
+  const dots = [];
+  let currentIndex = 0;
+
+  function updateButtons() {
+    prev.classList.remove('hidden');
+    next.classList.remove('hidden');
+
+    if (currentIndex === 0) {
+      prev.classList.add('hidden');
+    }
+    // if (currentIndex === 2) {
+    if (currentIndex === slides.length - 1) {
+      next.classList.add('hidden');
+    }
+  }
+
+  function moveSlides() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    // 画像はすべて同じ大きさのため、一枚目のスライドのwidthの情報を取得してslideWidthに代入する。それをテンプレートリテラルで値の挿入。
+    ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+  }
+
+  function setupDots() {
+    for (let i = 0; i < slides.length; i++) {
+      const button = document.createElement('button');
+      button.addEventListener('click', () => {
+        currentIndex = i;
+        updateDots();
+        updateButtons();
+        moveSlides();
+      });
+      dots.push(button);
+      document.querySelector('nav').appendChild(button);
+    }
+
+    dots[0].classList.add('current');
+  }
+
+  function updateDots() {
+    dots.forEach(dot => {
+      dot.classList.remove('current');
+    });
+    dots[currentIndex].classList.add('current');
+  }
+
+
+  updateButtons();
+  setupDots();
+
+  next.addEventListener('click', () => {
+    currentIndex++;
+    updateButtons();
+    updateDots();
+    moveSlides();
+  });
+
+  prev.addEventListener('click', () => {
+    currentIndex--;
+    updateButtons();
+    updateDots();
+    moveSlides();
+  });
+
+  window.addEventListener('resize', () => {
+    moveSlides();
+  });
 }
